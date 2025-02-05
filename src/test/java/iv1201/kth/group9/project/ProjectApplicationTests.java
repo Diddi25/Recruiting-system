acda639a99ff1cd4bc7077fe55ce4eab8ac18f4b
Sophie Malmberg <sophiema@kth.se>
2025-02-05 11:37:17 +0100

    Initial commit

---
+++ src/test/java/iv1201/kth/group9/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package iv1201.kth.group9.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
